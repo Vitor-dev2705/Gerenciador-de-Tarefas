4318caf1fa55eb8fc3b7cc5ddfab7ecd0fa4c521
anderson.teles <anderson.teles@sipam.gov.br>
2025-01-10 17:06:01 -0300

    feat: arrumando literal de estado

--- src/app/models.py
+++ src/app/models.py
@@ -1,16 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from datetime import datetime
 from typing import Optional
+from typing import Literal
+from fastapi import HTTPException
 
 class Tarefa(BaseModel):
     id: int
     titulo: str
     descricao: Optional[str] = None
-    estado: str
+    estado: Literal["pendente", "em andamento", "concluída"]  # Apenas esses valores são permitidos
     data_criacao: datetime
     data_atualizacao: datetime
+
+    @field_validator("estado")
+    def validar_estado(cls, valor):
+        if valor not in ["pendente", "em andamento", "concluída"]:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Estado '{valor}' não é um valor válido. Os valores aceitos são 'pendente', 'em andamento' e 'concluída'."
+            )
+        return valor
 
 class TarefaCriar(BaseModel):
     titulo: str
     descricao: Optional[str] = None
-    estado: str
+    estado: Literal["pendente", "em andamento", "concluída"]  # Apenas esses valores são permitidos
+
+    @field_validator("estado")
+    def validar_estado(cls, valor):
+        if valor not in ["pendente", "em andamento", "concluída"]:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Estado '{valor}' não é um valor válido. Os valores aceitos são 'pendente', 'em andamento' e 'concluída'."
+            )
+        return valor
